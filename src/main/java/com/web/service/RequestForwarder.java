bb6059e65c9de01a8db8f56e316f6b8733501fad
jkburlison96 <jkburlison96@gmail.com>
2020-10-30 02:56:12 -0500

    route update

--- src/main/java/com/web/service/RequestForwarder.java
+++ src/main/java/com/web/service/RequestForwarder.java
@@ -13,11 +13,11 @@ public class RequestForwarder {
 	
 	public String routes(HttpServletRequest req) {
 		switch (req.getRequestURI()){
-		case "./home.page":
+		case "/p1-0.0.1-SNAPSHOT/home.page":
 			return "html/home.html";
-		case "./reimbursementView.page":
+		case "/p1-0.0.1-SNAPSHOT/reimbursementView.page":
 			return "html/reimbursementView.html";
-		case "./requestReimbursement.page":
+		case "/p1-0.0.1-SNAPSHOT/requestReimbursement.page":
 			return "html/requestReimbursement.html";
 		default: 
 			return "html/login.html";
@@ -26,13 +26,13 @@ public class RequestForwarder {
 	
 	public void data(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		switch(req.getRequestURI()) {
-		case "/p1/allReimbursements.json":
+		case "/p1-0.0.1-SNAPSHOT/js/allReimbursements.json":
 			new ReimbursementDataController().sendAllData(res);
 			break;
-		case "/p1/reqReimbursement.json":
+		case "/p1-0.0.1-SNAPSHOT/js/reqReimbursement.json":
 			new SaveController().saveReimbursement(req, res);
 			break;
-		case "/p1/userLogin.json":
+		case "/p1-0.0.1-SNAPSHOT/js/userLogin.json":
 			new UserController().login(req, res);
 			break;
 		}
